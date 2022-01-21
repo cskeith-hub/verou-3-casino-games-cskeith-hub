f1ee3f5b7b09f4e04d0eb960175089301b8a2104
Rustiek <cskeith01@gmail.com>
2022-01-21 18:46:46 +0100

    Rework rock paper scissors

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,77 +1,74 @@
+// Create array
 const choiceArray = [
     {
         name: 'Rock',
-        picture: '<img src="./image/theRock200.jpg" alt="PLayer" width="200" height="200">',
-        paper: 'lose',
-        scissors: 'win',
-        rock: 'draw'
+        picture: '<img src="./image/theRock200.jpg" alt="rock" width="200" height="200">',
     },
 
     {
-        name: 'paper',
-        picture: '<img src="./image/paper.jfif" alt="PLayer" width="200" height="200">',
-        rock: 'win',
-        scissors: 'lose',
-        paper: 'draw'
+        name: 'Paper',
+        picture: '<img src="./image/paper.jfif" alt="paper" width="200" height="200">',
     },
 
     {
-        name: 'scissors',
-        picture: '<img src="./image/scissors200.jpg" alt="PLayer" width="200" height="200">',
-        rock: 'win',
-        paper: 'lose',
-        scissors: 'draw'
+        name: 'Scissors',
+        picture: '<img src="./image/scissors200.jpg" alt="scissors" width="200" height="200">',
     },
 ]
 
-
-
-const choice1 = document.getElementById("choice1");
-const choice2 = document.getElementById("choice2");
 const play = document.getElementById("play");
 const buttonRock = document.getElementById("rock");
 const buttonPaper = document.getElementById("paper");
 const buttonScissors = document.getElementById("scissors");
-console.log(play);
-buttonRock.addEventListener('click', function () {
-    event.preventDefault();
-    choice1.innerHTML = choiceArray[0].picture;
-
-
-})
-
-buttonPaper.addEventListener('click', function () {
-    event.preventDefault();
-    choice1.innerHTML = choiceArray[1].picture;
-
-
-})
-
-buttonScissors.addEventListener('click', function () {
-    event.preventDefault();
-    choice1.innerHTML = choiceArray[2].picture;
-
-
-})
+const playerChoice = document.getElementById("choice1");
+let computerResult = document.getElementById("choice2");
 
 play.addEventListener('click', function () {
-    event.preventDefault();
-    for (let i = 0; i < choiceArray.length; i++){
-       const randomP = Math.floor(Math.random() * choiceArray.length);
-       if (randomP === 0){
-           choice2.innerHTML = choiceArray[0].picture;
-       }if (randomP === 1){
-            choice2.innerHTML = choiceArray[1].picture;
-        }if (randomP === 2){
-            choice2.innerHTML = choiceArray[2].picture;
+    // todo fetch player choice + store variable
+    buttonRock.addEventListener('click', function (rock) {
+        event.preventDefault();
+        const playerChoiceRock = choiceArray[0].name;
+        playerChoice.innerHTML = choiceArray[0].picture;
+        computer();
+    })
+    buttonPaper.addEventListener('click', function () {
+        event.preventDefault();
+        const playerChoicePaper = choiceArray[1].name;
+        playerChoice.innerHTML = choiceArray[1].picture;
+        computer();
+    })
+    buttonScissors.addEventListener('click', function () {
+        event.preventDefault();
+        const playerChoiceScissors = choiceArray[2].name;
+        playerChoice.innerHTML = choiceArray[2].picture;
+        computer();
+    });
+    // todo fetch computer choice + store variable
+    const computer = () => {
+        // todo make an array with options  this is global
+        // todo generate random index for array
+        let randomChoice = Math.floor(Math.random() * choiceArray.length);
+        // todo create switch method for computer choices
+        const computerChoice = choiceArray[randomChoice];
+        switch (computerChoice.name) {
+            case "Rock":
+                computerResult.innerHTML = computerChoice.picture
+                break;
+            case "Paper":
+                computerResult.innerHTML = computerChoice.picture
+                break;
+            case "Scissors":
+                computerResult.innerHTML = computerChoice.picture
+                break;
+            default:
+                return
         }
     }
+    // todo comparing variables
+    // todo result
 
 
 })
-
-
-
 
 
 
